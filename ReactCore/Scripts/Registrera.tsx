377d2a92445fb248fbe00569ad982785d5e85fc3
Per Selin <pse@ptj.se>
2017-07-25 14:58:57 +0200

    fixes

--- ReactCore/Scripts/Registrera.tsx
+++ ReactCore/Scripts/Registrera.tsx
@@ -25,33 +25,43 @@ export class Registrera extends React.Component<IAppProps, IAppState> {
         this.setState(tempState);
 
     }
+    spara(e) {
+        
+    }
 
     render() {
       
         return (
             <div>
-                <h2>Registrera </h2>
-                <div className="form-group">
-                    <label className="control-label col-sm-2"> Leveransadress</label>
-                    <div className="col-sm-10">
-                        <input type="text" id="Leveransadress" value={this.state.Leveransadress} onChange={this.handleChange.bind(this)} className="form-control" />
-                    </div>
-                </div>
-                <br />
-                <div className="form-group">
-                    <label className="control-label col-sm-2"> Postnummer</label>
-                    <div className="col-sm-10">
-                        <input type="text" id="Postnummer" value={this.state.Postnummer} onChange={this.handleChange.bind(this)} className="form-control" /> 
-                    </div>
-                </div>
-                <br />
-                <div className="form-group">
-                    <label className="control-label col-sm-2"> Postadress</label>
-                    <div className="col-sm-10">
-                        <input type="text" id="Postadress"  value={this.state.Postadress} onChange={this.handleChange.bind(this)} className="form-control" />
-                        {this.state.ValideringFelPostadress ? <span ><i aria-hidden="true"> fel</i></span> : null}
-                    </div>
-                </div>
+                <h2>Registrera</h2>
+                <fieldset>
+                    <form className="form-horizontal" onSubmit={this.spara.bind(this)}>
+                        <div className="form-group">
+                            <label className="control-label col-sm-2"> Leveransadress</label>
+                            <div className="col-sm-10">
+                                <input type="text" id="Leveransadress" value={this.state.Leveransadress} onChange={this.handleChange.bind(this)} className="form-control" />
+                            </div>
+                        </div>
+                        
+                        <div className="form-group">
+                            <label className="control-label col-sm-2"> Postnummer</label>
+                            <div className="col-sm-10">
+                                <input type="text" id="Postnummer" value={this.state.Postnummer} onChange={this.handleChange.bind(this)} className="form-control" /> 
+                            </div>
+                        </div>
+                       
+                        <div className="form-group">
+                            <label className="control-label col-sm-2"> Postadress</label>
+                            <div className="col-sm-10">
+                                <input type="text" id="Postadress"  value={this.state.Postadress} onChange={this.handleChange.bind(this)} className="form-control" />
+                                {this.state.ValideringFelPostadress ? <span ><i aria-hidden="true"> fel</i></span> : null}
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <button type="submit"  className="btn btn-success btn-primary glyphicon-envelope margin-left12" > Spara </button>
+                        </div>
+                    </form>
+                </fieldset>   
             </div>
         );
     }
--- ReactCore/wwwroot/dist/bundle.js
+++ ReactCore/wwwroot/dist/bundle.js
@@ -8421,25 +8421,29 @@ class Registrera extends React.Component {
         tempState[event.target.id] = event.target.value;
         this.setState(tempState);
     }
+    spara(e) {
+    }
     render() {
         return (React.createElement("div", null,
-            React.createElement("h2", null, "Registrera "),
-            React.createElement("div", { className: "form-group" },
-                React.createElement("label", { className: "control-label col-sm-2" }, " Leveransadress"),
-                React.createElement("div", { className: "col-sm-10" },
-                    React.createElement("input", { type: "text", id: "Leveransadress", value: this.state.Leveransadress, onChange: this.handleChange.bind(this), className: "form-control" }))),
-            React.createElement("br", null),
-            React.createElement("div", { className: "form-group" },
-                React.createElement("label", { className: "control-label col-sm-2" }, " Postnummer"),
-                React.createElement("div", { className: "col-sm-10" },
-                    React.createElement("input", { type: "text", id: "Postnummer", value: this.state.Postnummer, onChange: this.handleChange.bind(this), className: "form-control" }))),
-            React.createElement("br", null),
-            React.createElement("div", { className: "form-group" },
-                React.createElement("label", { className: "control-label col-sm-2" }, " Postadress"),
-                React.createElement("div", { className: "col-sm-10" },
-                    React.createElement("input", { type: "text", id: "Postadress", value: this.state.Postadress, onChange: this.handleChange.bind(this), className: "form-control" }),
-                    this.state.ValideringFelPostadress ? React.createElement("span", null,
-                        React.createElement("i", { "aria-hidden": "true" }, " fel")) : null))));
+            React.createElement("h2", null, "Registrera"),
+            React.createElement("fieldset", null,
+                React.createElement("form", { className: "form-horizontal", onSubmit: this.spara.bind(this) },
+                    React.createElement("div", { className: "form-group" },
+                        React.createElement("label", { className: "control-label col-sm-2" }, " Leveransadress"),
+                        React.createElement("div", { className: "col-sm-10" },
+                            React.createElement("input", { type: "text", id: "Leveransadress", value: this.state.Leveransadress, onChange: this.handleChange.bind(this), className: "form-control" }))),
+                    React.createElement("div", { className: "form-group" },
+                        React.createElement("label", { className: "control-label col-sm-2" }, " Postnummer"),
+                        React.createElement("div", { className: "col-sm-10" },
+                            React.createElement("input", { type: "text", id: "Postnummer", value: this.state.Postnummer, onChange: this.handleChange.bind(this), className: "form-control" }))),
+                    React.createElement("div", { className: "form-group" },
+                        React.createElement("label", { className: "control-label col-sm-2" }, " Postadress"),
+                        React.createElement("div", { className: "col-sm-10" },
+                            React.createElement("input", { type: "text", id: "Postadress", value: this.state.Postadress, onChange: this.handleChange.bind(this), className: "form-control" }),
+                            this.state.ValideringFelPostadress ? React.createElement("span", null,
+                                React.createElement("i", { "aria-hidden": "true" }, " fel")) : null)),
+                    React.createElement("div", { className: "form-group" },
+                        React.createElement("button", { type: "submit", className: "btn btn-success btn-primary glyphicon-envelope margin-left12" }, " Spara "))))));
     }
 }
 exports.Registrera = Registrera;
